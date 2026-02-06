af1f6ba7612f032af68463e7c376e8d08e9763cb
Somon Karimzoda <karimzoda@example.com>
2026-02-06 23:09:57 +0500

    fix snow imports and paths

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import { startSnow } from "/src/snow/snow.js";
-import { showTreeIfWinter } from "/src/christmas tree/christmas tree.js";
-
+import { startSnow } from "./src/snow/snow.js";
+import { showTreeIfWinter } from "./src/christmas-tree/christmas-tree.js";
 startSnow();
 showTreeIfWinter();
